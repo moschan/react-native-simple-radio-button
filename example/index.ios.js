ddd0bedb2ef69ecf7ee6dc66648e6bec31bab29b
moschan <kaityo02@gmail.com>
2016-03-29 20:08:53 +0800

    add label left example

--- example/index.ios.js
+++ example/index.ios.js
@@ -25,6 +25,9 @@ var RadioButtonProject = React.createClass({
       types2: [{label: 'param1', value: 0}, {label: 'param2', value: 1}, {label: 'param3', value: 2},],
       value2: 0,
       value2Index: 0,
+      types3: [{label: 'param1', value: 0}, {label: 'param2', value: 1}, {label: 'param3', value: 2},],
+      value3: 0,
+      value3Index: 0,
     }
   },
   render: function() {
@@ -65,10 +68,10 @@ var RadioButtonProject = React.createClass({
                     isSelected={is_selected}
                     obj={obj}
                     index={i}
-                    labelHorizontal={false}
+                    labelHorizontal={true}
                     buttonColor={'#2196f3'}
                     labelColor={'#000'}
-                    style={i !== this.state.types2.length-1 && styles.radioStyle}
+                    style={[i !== this.state.types2.length-1 && styles.radioStyle]}
                     onPress={(value, index) => {
                       this.setState({value2:value})
                       this.setState({value2Index: index});
@@ -80,6 +83,37 @@ var RadioButtonProject = React.createClass({
           </Radio>
         </View>
         <Text>selected: {this.state.types2[this.state.value2Index].label}</Text>
+
+        <Text style={styles.welcome}>3. label left example</Text>
+        <View style={styles.component}>
+          <Radio
+            formHorizontal={false}
+            animation={true}
+          >
+            {this.state.types3.map((obj, i) => {
+              var that = this;
+              var is_selected = this.state.value3Index == i;
+              return (
+                <View key={i} style={styles.radioButtonWrap}>
+                  <RadioButton
+                    isSelected={is_selected}
+                    obj={obj}
+                    index={i}
+                    labelHorizontal={true}
+                    labelStyle={{position: 'absolute', left: -35}}
+                    buttonColor={'#2196f3'}
+                    labelColor={'#000'}
+                    onPress={(value, index) => {
+                      this.setState({value3:value})
+                      this.setState({value3Index: index});
+                    }}
+                  />
+                </View>
+              )
+            })}
+          </Radio>
+        </View>
+        <Text>selected: {this.state.types3[this.state.value3Index].label}</Text>
 
       </View>
     );
